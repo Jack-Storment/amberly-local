86e7517b983fdca3fd30869dc999cb24e3d19e41
Svapnil <svapnila@gmail.com>
2020-03-14 16:52:32 -0400

    deleted products page and change wine menu title

--- src/pages/wine-menu.js
+++ src/pages/wine-menu.js
@@ -16,13 +16,15 @@ export const WinePageTemplate = () => {
             }}
             >
                 <h1
-                    className="has-text-weight-bold is-size-1"
-                    style={{
-                    boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-                    backgroundColor: '#f40',
+                  className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
+                  style={{
+                    boxShadow:
+                      'rgb(0, 0, 0) 0.5rem 0px 0px, rgb(0, 0, 0) -0.5rem 0px 0px',
+                    backgroundColor: 'rgb(0, 0, 0)',
                     color: 'white',
-                    padding: '1rem',
-                    }}
+                    lineHeight: '1',
+                    padding: '0.25em',
+                  }}
                 >
                     Wine Menu
                 </h1>
