cd2541079636d3412364bdde9f639fb0073550f6
Svapnil <svapnila@gmail.com>
2020-04-03 18:45:22 -0400

    removed background blocking

--- src/pages/wine-menu.js
+++ src/pages/wine-menu.js
@@ -18,9 +18,6 @@ export const WinePageTemplate = () => {
                 <h1
                   className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
                   style={{
-                    boxShadow:
-                      'rgb(0, 0, 0) 0.5rem 0px 0px, rgb(0, 0, 0) -0.5rem 0px 0px',
-                    backgroundColor: 'rgb(0, 0, 0)',
                     color: 'white',
                     lineHeight: '1',
                     padding: '0.25em',
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -38,25 +38,21 @@ export const IndexPageTemplate = ({
         <h1
           className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
           style={{
-            boxShadow:
-              'rgb(0, 0, 0) 0.5rem 0px 0px, rgb(0, 0, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(0, 0, 0)',
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',
+            textAlign: 'center'
           }}
         >
-          {title}
+          {title}<br/>{"AMBERLY LOCAL"}
         </h1>
         <h3
           className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
           style={{
-            boxShadow:
-              'rgb(0, 0, 0) 0.5rem 0px 0px, rgb(0, 0, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(0, 0, 0)',
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',
+            textAlign: 'center',
           }}
         >
           {subheading}
