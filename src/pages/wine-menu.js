e55025a9f05b4517160284f55a4f31cb12ed4f32
Jack Storment <41656084+Jack-Storment@users.noreply.github.com>
2020-04-04 17:13:05 -0400

    Changed font color of Wine Menu tab

--- src/pages/wine-menu.js
+++ src/pages/wine-menu.js
@@ -18,7 +18,7 @@ export const WinePageTemplate = () => {
                 <h1
                   className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
                   style={{
-                    color: 'white',
+                    color: 'black',
                     lineHeight: '1',
                     padding: '0.25em',
                   }}
