433ff4048cb94f3213d365def74b34162a8f86b3
Triangle Web Solutions <60792166+TriangleWebSolutions@users.noreply.github.com>
2020-04-06 17:02:39 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,14 @@ module.exports = {
         name: 'uploads',
       },
     },
-    
+          {
+       resolve: "gatsby-plugin-web-font-loader",
+       options: {
+          custom: {
+            families: ["Lulo"],
+            urls: ["/static/fonts/fonts.css"],
+          },
+       },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
@@ -23,15 +30,7 @@ module.exports = {
         name: 'pages',
       },
     },
-      {
-          resolve: "gatsby-plugin-web-font-loader",
-          options: {
-            custom: {
-              families: ["Lato, Dank Mono"],
-              urls: ["/fonts/fonts.css"],
-            },
-          },
-        },
+     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
