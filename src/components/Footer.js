c681aeef440e60671f6da42d8c2756f132cb1811
Jack-Storment <jstorment1998@gmail.com>
2020-04-24 14:30:33 -0400

    Updates to Navbar (added Lulo bold font)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import {
+  Link
+} from 'gatsby'
 
 import localBuilding from '../img/localbuilding.jpg'
 import facebook from '../img/social/facebook.svg'
@@ -9,90 +11,139 @@ import vimeo from '../img/social/vimeo.svg'
 
 const Footer = class extends React.Component {
   render() {
-    return (
-      <footer className="footer">
-        <div className="content has-text-centered">
-          <img
-            src={localBuilding}
-            alt="Amberly Local"
-            style={{ width: '14em' }}
-          />
-        </div>
-        <div className="content has-text-centered">
-          <div className="container">
-            <div className="columns">
-              <div className="column is-4">
-                <section className="menu">
-                  <ul className="menu-list">
-                    <li>
-                      <Link to="/" className="navbar-item">
-                        Home
-                      </Link>
-                    </li>
-                    <li>
-                      <a className="navbar-item" href="https://direct.chownow.com/order/17058/locations/24351">
-                        Order Online
-                      </a>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/events">
-                        Events
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/about">
-                        Team
-                      </Link>
-                    </li>
-                  </ul>
-                </section>
-              </div>
-              <div className="column is-4">
-                <section>
-                  <ul className="menu-list">
-                    <li>
-                      <Link className="navbar-item" to="/beer-menu">
-                        Draft List
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/wine-menu">
-                        Wine List
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/menu">
-                        Food Menu
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/catering">
-                        Catering
-                      </Link>
-                    </li>
-                  </ul>
-                </section>
-              </div>
-              <div className="column is-4 social">
-                <a title="facebook" href="https://www.facebook.com/amberlylocal/">
-                  <img
-                    src={facebook}
-                    alt="Facebook"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="instagram" href="https://www.instagram.com/amberly_local/">
-                  <img
-                    src={instagram}
-                    alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </footer>
+    return ( <
+      footer className = "footer" >
+      <
+      div className = "content has-text-centered" >
+      <
+      img src = {
+        localBuilding
+      }
+      alt = "Amberly Local"
+      style = {
+        {
+          width: '14em'
+        }
+      }
+      /> <
+      /div> <
+      div className = "content has-text-centered" >
+      <
+      div className = "container" >
+      <
+      div className = "columns" >
+      <
+      div className = "column is-4" >
+      <
+      section className = "menu" >
+      <
+      ul className = "menu-list" >
+      <
+      li >
+      <
+      Link to = "/"
+      className = "navbar-item" >
+      Home <
+      /Link> <
+      /li> <
+      li >
+      <
+      a className = "navbar-item"
+      href = "https://direct.chownow.com/order/17058/locations/24351" >
+      Order Online <
+      /a> <
+      /li> <
+      li >
+      <
+      Link className = "navbar-item"
+      to = "/events" >
+      Events <
+      /Link> <
+      /li> <
+      li >
+      <
+      Link className = "navbar-item"
+      to = "/about" >
+      About us <
+      /Link> <
+      /li> <
+      /ul> <
+      /section> <
+      /div> <
+      div className = "column is-4" >
+      <
+      section >
+      <
+      ul className = "menu-list" >
+      <
+      li >
+      <
+      Link className = "navbar-item"
+      to = "/beer-menu" >
+      Draft List <
+      /Link> <
+      /li> <
+      li >
+      <
+      Link className = "navbar-item"
+      to = "/wine-menu" >
+      Wine List <
+      /Link> <
+      /li> <
+      li >
+      <
+      Link className = "navbar-item"
+      to = "/menu" >
+      Food Menu <
+      /Link> <
+      /li> <
+      li >
+      <
+      Link className = "navbar-item"
+      to = "/catering" >
+      Catering <
+      /Link> <
+      /li> <
+      /ul> <
+      /section> <
+      /div> <
+      div className = "column is-4 social" >
+      <
+      a title = "facebook"
+      href = "https://www.facebook.com/amberlylocal/" >
+      <
+      img src = {
+        facebook
+      }
+      alt = "Facebook"
+      style = {
+        {
+          width: '1em',
+          height: '1em'
+        }
+      }
+      /> <
+      /a> <
+      a title = "instagram"
+      href = "https://www.instagram.com/amberly_local/" >
+      <
+      img src = {
+        instagram
+      }
+      alt = "Instagram"
+      style = {
+        {
+          width: '1em',
+          height: '1em'
+        }
+      }
+      /> <
+      /a> <
+      /div> <
+      /div> <
+      /div> <
+      /div> <
+      /footer>
     )
   }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import {
+  Link
+} from 'gatsby'
 import logo from '../img/fulllogo.jpg'
 
 const Navbar = class extends React.Component {
@@ -13,80 +15,108 @@ const Navbar = class extends React.Component {
 
   toggleHamburger = () => {
     // toggle the active boolean in the state
-    this.setState(
-      {
+    this.setState({
         active: !this.state.active,
       },
       // after state has been updated,
       () => {
         // set the class in state for the navbar accordingly
-        this.state.active
-          ? this.setState({
-              navBarActiveClass: 'is-active',
-            })
-          : this.setState({
-              navBarActiveClass: '',
-            })
+        this.state.active ?
+          this.setState({
+            navBarActiveClass: 'is-active',
+          }) :
+          this.setState({
+            navBarActiveClass: '',
+          })
       }
     )
   }
 
   render() {
-    return (
-      <nav
-        className="navbar is-transparent"
-        role="navigation"
-        aria-label="main-navigation"
-      >
-        <div className="container">
-          <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '65px' }} />
-            </Link>
-            {/* Hamburger menu */}
-            <div
-              className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-              data-target="navMenu"
-              onClick={() => this.toggleHamburger()}
-            >
-              <span />
-              <span />
-              <span />
-            </div>
-          </div>
-          <div
-            id="navMenu"
-            className={`navbar-menu ${this.state.navBarActiveClass}`}
-          >
-            <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/">
-                Home
-              </Link>
-              <Link className="navbar-item" to="/beer-menu">
-                Draft List
-              </Link>
-              <Link className="navbar-item" to="/wine-menu">
-                Wine List
-              </Link>
-              <Link className="navbar-item" to="/menu">
-                Food Menu
-              </Link>
-              <a className="navbar-item" href="https://direct.chownow.com/order/17058/locations/24351">
-                Order Online
-              </a>
-              <Link className="navbar-item" to="/catering">
-                Catering
-              </Link>
-              <Link className="navbar-item" to="/events">
-                Events
-              </Link>
-              <Link className="navbar-item" to="/about">
-                Team
-              </Link>
-            </div>    
-          </div>
-        </div>
-      </nav>
+    return ( <
+      nav className = "navbar is-transparent"
+      role = "navigation"
+      aria - label = "main-navigation" >
+      <
+      div className = "container" >
+      <
+      div className = "navbar-brand" >
+      <
+      Link to = "/"
+      className = "navbar-item"
+      title = "Logo" >
+      <
+      img src = {
+        logo
+      }
+      alt = "Kaldi"
+      style = {
+        {
+          width: '65px'
+        }
+      }
+      /> <
+      /Link> {
+        /* Hamburger menu */ } <
+      div className = {
+        `navbar-burger burger ${this.state.navBarActiveClass}`
+      }
+      data - target = "navMenu"
+      onClick = {
+        () => this.toggleHamburger()
+      } >
+      <
+      span / >
+      <
+      span / >
+      <
+      span / >
+      <
+      /div> <
+      /div> <
+      div id = "navMenu"
+      className = {
+        `navbar-menu ${this.state.navBarActiveClass}`
+      } >
+      <
+      div className = "navbar-start has-text-centered" >
+      <
+      Link className = "navbar-item"
+      to = "/" >
+      Home <
+      /Link> <
+      Link className = "navbar-item"
+      to = "/beer-menu" >
+      Draft List <
+      /Link> <
+      Link className = "navbar-item"
+      to = "/wine-menu" >
+      Wine List <
+      /Link> <
+      Link className = "navbar-item"
+      to = "/menu" >
+      Food Menu <
+      /Link> <
+      a className = "navbar-item"
+      href = "https://direct.chownow.com/order/17058/locations/24351" >
+      Order Online <
+      /a> <
+      Link className = "navbar-item"
+      to = "/catering" >
+      Catering <
+      /Link> <
+      Link className = "navbar-item"
+      to = "/events" >
+      Events <
+      /Link> <
+      Link className = "navbar-item"
+      to = "/about" >
+      About us <
+      /Link> <
+      /div>     <
+      /div> <
+      /div> <
+      /nav>
     )
   }
 }
