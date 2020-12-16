46e63a9fa4c566a628cc0fc5faa255244d1b3ebb
Jack Storment <jstorment1998@gmail.com>
2020-12-16 14:31:16 -0500

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -44,7 +44,7 @@ export const IndexPageTemplate = ({
             textAlign: 'center'
           }}
         >
-          {title}<br/>{"AMBERLY LOCAL"}
+          {title}<br/>{"CONNECT REAL ESTATE"}
         </h1>
         <h3
           className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
